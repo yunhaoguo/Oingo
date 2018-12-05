eb15524dba3e9b9ad4cec0b1528d81f2d54c3775
yunhaoguo <929394263@qq.com>
2018-12-05 01:44:38 -0500

    add login and register function

--- app/src/main/java/com/yunhaoguo/oingo/activity/LoginActivity.java
+++ app/src/main/java/com/yunhaoguo/oingo/activity/LoginActivity.java
@@ -3,14 +3,30 @@ package com.yunhaoguo.oingo.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yunhaoguo.oingo.MainActivity;
 import com.yunhaoguo.oingo.R;
+import com.yunhaoguo.oingo.utils.HttpUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -49,13 +65,59 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 startActivity(new Intent(this, RegisterActivity.class));
                 break;
             case R.id.btn_login:
-                String name = etUsername.getText().toString().trim();
-                String password = etPassword.getText().toString().trim();
-                startActivity(new Intent(this, MainActivity.class));
+                String name = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                Log.i("name", name);
+                Log.i("password", password);
+                verify(name, password);
                 break;
             case R.id.tv_forget_password:
                 break;
         }
+    }
+
+    private void verify(String name, String password) {
+        if (TextUtils.isEmpty(name) || TextUtils.isEmpty(password)) {
+            Toast.makeText(LoginActivity.this, "Wrong username or password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        OkHttpClient client = new OkHttpClient();
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("userName", name);
+            obj.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), obj.toString());
+        Request request = new Request.Builder().url(HttpUtils.LOGIN_URL).post(requestBody).build();
+        Response response = null;
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(okhttp3.Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(okhttp3.Call call, Response response) throws IOException {
+                try {
+                    JSONObject responseObj = new JSONObject(response.body().string());
+                    if (responseObj.getString("result").equals("SUCCESS")) {
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        finish();
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(LoginActivity.this, "Wrong username or password!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 
---
+++ app/src/main/java/com/yunhaoguo/oingo/utils/HttpUtils.java
@@ -0,0 +1,16 @@
+package com.yunhaoguo.oingo.utils;
+/*
+ * 项目名:     Oingo
+ * 包名:      com.yunhaoguo.oingo.utils
+ * 文件名:     HttpUtils
+ * 创建者:     yunhaoguo
+ * 创建时间:    2018/12/5 12:45 AM
+ * 描述:      连接后端所需常量
+ */
+
+
+public class HttpUtils {
+    public static final String INITIAL_URL = "http://10.0.2.2:8080/";
+    public static final String LOGIN_URL = INITIAL_URL + "login";
+    public static final String REGISTER_URL = INITIAL_URL + "register";
+}
