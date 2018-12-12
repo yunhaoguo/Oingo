84cf236f0a42de369f5a15b48c4d79f862b9401c
yunhaoguo <929394263@qq.com>
2018-12-12 02:03:40 -0500

    update .gitignore

---
+++ App/Oingo/app/src/main/java/com/yunhaoguo/oingo/activity/RegisterActivity.java
@@ -0,0 +1,104 @@
+package com.yunhaoguo.oingo.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.yunhaoguo.oingo.R;
+import com.yunhaoguo.oingo.utils.QueryUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private EditText etConfirmPass;
+    private EditText etEmail;
+    private Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        initView();
+    }
+
+    private void initView() {
+        etUsername = findViewById(R.id.et_register_username);
+        etPassword = findViewById(R.id.et_register_password);
+        etConfirmPass = findViewById(R.id.et_register_confirm_password);
+        etEmail = findViewById(R.id.et_register_email);
+        btnRegister = findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_register:
+                //获取输入框的值
+                String name = etUsername.getText().toString().trim();
+                String pass = etPassword.getText().toString().trim();
+                String confirmPass = etConfirmPass.getText().toString().trim();
+                String email = etEmail.getText().toString().trim();
+                if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(pass) &&
+                        !TextUtils.isEmpty(confirmPass) && !TextUtils.isEmpty(email)) {
+                    if (!pass.equals(confirmPass)) {
+                        Toast.makeText(this, "Two inputs of the password should be the same", Toast.LENGTH_SHORT).show();
+                    } else {
+                        register(name, pass, email);
+                    }
+                }
+                break;
+        }
+    }
+
+    private void register(String name, String pass, String email) {
+        QueryUtils.register(name, pass, email, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    JSONObject responseObj = new JSONObject(response.body().string());
+                    if (responseObj.getString("result").equals("SUCCESS")) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(RegisterActivity.this, "register success", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                        finish();
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(RegisterActivity.this, "create user failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
